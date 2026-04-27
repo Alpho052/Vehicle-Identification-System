018ec1b3e79d3a134201a6411c738fa86fdd6b83
Tankiso Alphonci Ramabolu <ramabolutankiso1@gmail.com>
2026-04-27 18:26:23 +0200

    Remove comment header from InsuranceDAO.java

--- src/main/java/com/vehicle/vehicleidentificationsystem/dao/InsuranceDAO.java
+++ src/main/java/com/vehicle/vehicleidentificationsystem/dao/InsuranceDAO.java
@@ -1,4 +1,3 @@
-// InsuranceDAO.java - src/main/java/com/vehicle/vehicleidentificationsystem/dao/InsuranceDAO.java
 package com.vehicle.vehicleidentificationsystem.dao;
 
 import com.vehicle.vehicleidentificationsystem.model.InsuranceRecord;
